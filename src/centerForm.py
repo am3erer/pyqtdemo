7c6b80c145800f795343c1de3ad1bd9a34ff7c21
Am3erer <tangsiweiwork@163.com>
2020-07-26 15:23:22 +0800

    通过代码创建窗体 并改变窗体出现的位置

---
+++ src/centerForm.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow,QApplication,QDesktopWidget
+from PyQt5.QtGui import QIcon
+
+class centerForm(QMainWindow):
+    def __init__(self,parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle('让窗口居中')
+        self.resize(400,300)
+        self.center()
+
+    def center(self):
+        #获取屏幕坐标系
+        screen = QDesktopWidget().screenGeometry()
+        #获取窗口坐标系
+        size = self.geometry()
+        newLeft = (screen.width() - size.width()) / 2
+        newTop = (screen.height() - size.height()) / 2
+        self.move(newLeft,newTop)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('../images/龙.png'))
+    main = centerForm()
+    main.show()
+    sys.exit(app.exec_())
