635af23b12141c76c2706f56f56d30b584e2dd78
Am3erer <tangsiweiwork@163.com>
2020-07-26 11:12:46 +0800

    试了试web engine 老版本是webviewer控件 生成的代码有问题 但我新的版本没问题 不需要修改生成的代码

---
+++ src/runweb.py
@@ -0,0 +1,12 @@
+from designer import web
+import sys
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = web.Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
